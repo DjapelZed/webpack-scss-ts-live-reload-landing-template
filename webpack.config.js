6a6e5b8a01b40d48f162ee4864c0907c830e45de
Max Kushnirov <max@mineable.io>
2023-05-08 11:51:24 +0300

    Webpack + SCSS + TS + Live Reload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    target: 'web',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        watchFiles: path.join(__dirname, 'src/**')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
